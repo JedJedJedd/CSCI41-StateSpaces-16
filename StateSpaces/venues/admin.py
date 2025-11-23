31fb631817715e7ed3767cd34a64003bb2210fc9
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-11-23 20:45:10 +0800

    added admin.py for venues

--- StateSpaces/venues/admin.py
+++ StateSpaces/venues/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Venue, Building, Amenity
 
-# Register your models here.
+
+class VenueInline(admin.TabularInline):
+    model = Venue
+
+
+class BuildingInline(admin.TabularInline):
+    model = Building
+
+
+class AmenityInline(admin.TabularInline):
+    model = Amenity
+
+
+class VenueAdmin(admin.ModelAdmin):
+    inlines = [BuildingInline, AmenityInline]
+
+
+
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Building)
+admin.site.register(Amenity)
