6bd5c5e3b9202accf03f3a2979ab9de84115950e
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-11-27 21:55:19 +0800

    committed accounts/forms.py and updated code to be cleaner

--- StateSpaces/accounts/forms.py
+++ StateSpaces/accounts/forms.py
@@ -5,10 +5,49 @@ from .models import AgentProfile, Team
 
 class CreateUserForm(UserCreationForm):
     #display_name = forms.CharField(max_length=63)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+        })
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'First Name',
+        })
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Last Name',
+        })
+    )
     birth_date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date'})
-        )
+        widget=forms.DateInput(attrs={
+            'type': 'date',
+        })
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Email Address',
+        })
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+        })
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password Again',
+        })
+    )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control border border-dark border-1', 'style': 'background-color: #F4EFE6;'})
+    
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'birth_date', 'email','password1', 'password2']
