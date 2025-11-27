856ff37b03b374f20c7216f1053bd876018fa172
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-11-28 00:41:22 +0800

    changed reservations admin

--- StateSpaces/reservations/admin.py
+++ StateSpaces/reservations/admin.py
@@ -1,14 +1,2 @@
 from django.contrib import admin
-from .models import Reservation
-
-
-class ReservationInline(admin.TabularInline):
-    model = Reservation
-
-
-class ReservationAdmin(admin.ModelAdmin):
-    inlines = [ReservationInline]
-
-
-
-admin.site.register(Reservation, ReservationAdmin)
+from .models import Reservation, Venue
--- StateSpaces/venues/admin.py
+++ StateSpaces/venues/admin.py
@@ -1,24 +1,16 @@
 from django.contrib import admin
-from .models import Venue, Building, Amenity
+from .models import Venue, Building
 
 
 class VenueInline(admin.TabularInline):
     model = Venue
 
 
-class BuildingInline(admin.TabularInline):
-    model = Building
 
-
-class AmenityInline(admin.TabularInline):
-    model = Amenity
-
-
-class VenueAdmin(admin.ModelAdmin):
-    inlines = [BuildingInline, AmenityInline]
+class BuildingAdmin(admin.ModelAdmin):
+    inlines = [VenueInline]
 
 
 
-admin.site.register(Venue, VenueAdmin)
-admin.site.register(Building)
-admin.site.register(Amenity)
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Venue)
