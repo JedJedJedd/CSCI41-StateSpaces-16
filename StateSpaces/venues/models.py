81f04db6da0754fb1c3d7c1abefa4b41ed7e7cb8
hannahdenisebacomo@gmail.com <hannahdenisebacomo@gmail.com>
2025-11-28 23:00:23 +0800

    removed district from building model

--- StateSpaces/venues/models.py
+++ StateSpaces/venues/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Building(models.Model):
     building_name = models.CharField(max_length=255)
     street = models.CharField(max_length=255)
-    district = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     
     def __str__(self):
