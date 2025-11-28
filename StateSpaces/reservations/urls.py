b262c17e503fd828863cab1351f2b5f14ab9fe44
NotAquazin <abdulnaina9@gmail.com>
2025-11-29 01:17:34 +0800

    fixed reservation link

--- StateSpaces/reservations/urls.py
+++ StateSpaces/reservations/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', ReservationListView.as_view(), name='reservation'),
     #path('<int:pk>/confirmation/', ReservationDetailView.as_view(), name='confirmation'),
     #commented out detail view above temporarily while we dont have data set
-    path('confirmation/', ConfirmationListView.as_view(), name='reservation'),
+    path('confirmation/', ConfirmationListView.as_view(), name='reservation-confirmation'),
 
 
 ]
