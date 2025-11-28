d1f58c0b85b4fe0c38013278081f27043c742df1
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-11-28 17:27:09 +0800

    Added the DetailView for venues

--- StateSpaces/venues/views.py
+++ StateSpaces/venues/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Venue
 # Create your views here.
 class VenuesListView(ListView):
@@ -9,3 +10,13 @@ class VenuesListView(ListView):
 class VenuesSearchListView(ListView):
      model = Venue
      template_name = 'venues/search_venues.html'     
+
+class VenueDetailView(DetailView):
+     model = Venue
+     template_name = 'venues/venue_detail.html'
+
+     def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["amenities"] = self.object.amenityassignment_set.select_related("amenity")
+        return ctx
+     
