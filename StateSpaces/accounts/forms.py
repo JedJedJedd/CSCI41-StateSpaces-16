7260bd295d325aff911e60abb2fe5e8e1613f158
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-11-29 22:51:28 +0800

    Updated forms.py to match PEP8 standards

--- StateSpaces/accounts/forms.py
+++ StateSpaces/accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import AgentProfile, Team
 
 class CreateUserForm(UserCreationForm):
+    """Form for registering a new user."""
     username = forms.CharField(
         widget=forms.TextInput(attrs={
             'placeholder': 'Username',
@@ -43,16 +44,22 @@ class CreateUserForm(UserCreationForm):
     )
 
     def __init__(self, *args, **kwargs):
+        """Adds Bootstrap implementation to all fields"""
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control border border-dark border-1', 'style': 'background-color: #F4EFE6;'})
+            field.widget.attrs.update({
+                'class': 'form-control border border-dark border-1', 
+                'style': 'background-color: #F4EFE6;'
+            })
     
     class Meta:
         model = User
+        #lists fields to show on form
         fields = ['username', 'first_name', 'last_name', 'birth_date', 'email','password1', 'password2']
 
 
 class CreateAgentForm(UserCreationForm):
+    """Form for registering a new agent"""
     agent_name = forms.CharField(
         max_length=100,
         widget=forms.TextInput(attrs={
@@ -92,12 +99,14 @@ class CreateAgentForm(UserCreationForm):
     )
 
     def __init__(self, *args, **kwargs):
+        """Adds Bootstrap implementation to all fields"""
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control border border-dark border-1', 'style': 'background-color: #F4EFE6;'})
 
     class Meta:
         model = User
+        #lists fields to show on form
         fields = [
             'username', 'first_name', 'last_name', 'email', 
             'agent_name', 'contact_number', 'team',
@@ -105,6 +114,7 @@ class CreateAgentForm(UserCreationForm):
         ]
 
     def save(self, commit=True):
+        """Save the User and create a related AgentProfile instance."""
         user = super().save(commit=commit)
         AgentProfile.objects.create(
             user=user,
